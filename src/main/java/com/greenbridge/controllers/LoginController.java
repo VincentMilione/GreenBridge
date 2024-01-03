c6ca78d960ba3fa2049b3abbbd2cfc1412599919
marmic98 <michelmartino98.mm@gmail.com>
2024-01-03 10:15:55 +0100

    login cliente

---
+++ src/main/java/com/greenbridge/controllers/LoginController.java
@@ -0,0 +1,28 @@
+package com.greenbridge.controllers;
+
+import com.greenbridge.entities.Cliente;
+import com.greenbridge.services.ClienteServiceImpl;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+    @Autowired
+    ClienteServiceImpl clienteService;
+
+    @PostMapping("/loginCliente")
+    public ResponseEntity<String> saveCliente(@RequestBody Cliente cliente, HttpSession session){
+        Cliente c = clienteService.getClienteByEmail(cliente.getEmail());
+        if (c != null && cliente.getPassword().compareTo(c.getPassword()) == 0) {
+            session.setAttribute("cliente", c);
+            return new ResponseEntity<>("ok",HttpStatus.OK);
+        }
+        return new ResponseEntity<>("notok", HttpStatus.FORBIDDEN);
+    }
+
+}
