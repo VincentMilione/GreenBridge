2d63970272d666adee629cc5306f8f94645291d7
MauroP001 <146085892+MauroP001@users.noreply.github.com>
2024-01-03 17:18:21 +0100

    Add files via upload
    Tabelle del database greenbridgedb

---
+++ tabelle.sql
@@ -0,0 +1,163 @@
+create table portafoglio(
+id_portafoglio int primary key auto_increment,
+credito float,
+limite_massimo int default 5000
+);
+
+create table agricoltore(
+id_agricoltore int primary key auto_increment,
+nome varchar(40) not null,
+email varchar(50) not null,
+pwd varchar(16) not null,
+nome_bottega varchar(30) not null,
+id_portafoglio int,
+foreign key(id_portafoglio) references portafoglio(id_portafoglio)
+ON update cascade
+on delete cascade
+);
+
+create table amministratore(
+id_admin int primary key auto_increment,
+username varchar(10) not null,
+pwd varchar(16) not null
+);
+
+create table certificato(
+id_certificato int primary key auto_increment,
+nome varchar(20) not null,
+data_scadenza date not null,
+scansione mediumblob not null,
+id_agricoltore int,
+foreign key(id_agricoltore) references agricoltore (id_agricoltore)
+ON UPDATE CASCADE
+ON DELETE CASCADE,
+id_admin int NULL,
+foreign key(id_admin) references amministratore (id_admin)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
+
+create table prodotto(
+id_prodotto int primary key AUTO_INCREMENT,
+nome varchar(20) not null,
+origine varchar(20) not null,
+immagine mediumblob,
+formato_vendita float not null,
+prezzo_kg float not null,
+prezzo_vendita float not null,
+quantita_disp float not null,
+lotto int,
+descrizione varchar(100),
+acquistabile bool default 1,
+id_agricoltore int,
+foreign key(id_agricoltore) references agricoltore(id_agricoltore)
+ON UPDATE CASCADE
+ON DELETE CASCADE
+);
+
+create table cliente(
+id_cliente int primary key auto_increment,
+nome varchar(30) not null,
+cognome varchar(30) not null,
+pwd varchar(16) not null,
+data_nascita date not null,
+email varchar(30) not null
+);
+
+create table ordine(
+id_ordine int primary key auto_increment,
+importo float not null,
+data_ordine date not null,
+pagamento varchar(20),
+id_cliente int NULL,
+foreign key(id_cliente) references cliente(id_cliente)
+on update cascade
+on delete set null,
+id_agricoltore int NULL,
+foreign key(id_agricoltore) references agricoltore(id_agricoltore)
+on update cascade
+on delete set null
+);
+
+create table prodotti_ordine(
+id_prodotto int,
+id_ordine int,
+kg_acquistati int,
+foreign key(id_prodotto) references prodotto(id_prodotto)
+on update cascade
+on delete no action,
+foreign key(id_ordine) references ordine(id_ordine)
+on update cascade
+on delete cascade,
+primary key(id_prodotto,id_ordine)
+);
+
+create table fattura(
+id_fattura int primary key auto_increment,
+data_emissione date not null,
+id_ordine int,
+foreign key(id_ordine) references ordine(id_ordine)
+on update cascade
+on delete no action,
+id_cliente int,
+foreign key(id_cliente) references cliente(id_cliente)
+on update cascade
+on delete no action
+);
+
+create table recensione_agricoltore(
+id_cliente int,
+id_agricoltore int,
+descrizione varchar(30) not null,
+voto int not null,
+foreign key(id_agricoltore) references agricoltore(id_agricoltore)
+on update cascade
+on delete cascade,
+foreign key(id_cliente) references cliente(id_cliente)
+on update cascade
+on delete cascade,
+primary key(id_cliente,id_agricoltore)
+);
+
+create table carrello_cliente(
+id_cliente int,
+id_prodotto int,
+kg_richiesti int not null,
+foreign key(id_cliente) references cliente(id_cliente)
+on update cascade
+on delete cascade,
+foreign key(id_prodotto) references prodotto(id_prodotto)
+on update cascade
+on delete cascade,
+primary key(id_cliente,id_prodotto)
+);
+
+create table recensione_prodotti(
+id_cliente int,
+id_prodotto int,
+descrizione varchar(30) not null,
+voto int not null,
+foreign key(id_cliente) references cliente(id_cliente)
+on update cascade
+on delete cascade,
+foreign key(id_prodotto) references prodotto(id_prodotto)
+on update cascade
+on delete cascade,
+primary key(id_cliente,id_prodotto)
+);
+
+create table indirizzo_spedizione(
+id_cliente int,
+id_indirizzo int primary key auto_increment,
+civico int not null,
+via varchar(20) not null,
+cap int not null,
+citta varchar(20) not null,
+paese varchar(20) not null,
+foreign key(id_cliente) references cliente(id_cliente)
+on update cascade
+on delete cascade
+);
+
+
+
