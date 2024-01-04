ab38d6158e76fb7adad62e4a40ddf1b356f989e5
Giovanni De Gregorio <degregorio176@gmail.com>
2024-01-04 16:50:18 +0100

    Controllo sessione chatbot aggiunto

--- src/main/java/com/greenbridge/controllers/ChatbotController.java
+++ src/main/java/com/greenbridge/controllers/ChatbotController.java
@@ -1,7 +1,10 @@
 package com.greenbridge.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ChatbotController {
@@ -11,5 +14,8 @@ public class ChatbotController {
     }
 
     @GetMapping("/")
-    public String showHome(){return "/pages/user/home";}
+    public String showHome(Model model, HttpSession session){
+        session.setAttribute("cliente", "true");
+        return "/pages/user/home";
+    }
 }
