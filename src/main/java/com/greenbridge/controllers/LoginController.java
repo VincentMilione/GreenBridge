14fa4c68639d32fe23ea5e70acddf623cf003c9b
marmic98 <michelmartino98.mm@gmail.com>
2024-01-07 11:26:17 +0100

    Prima revisione autonoma dello stile

--- src/main/java/com/greenbridge/controllers/LoginController.java
+++ src/main/java/com/greenbridge/controllers/LoginController.java
@@ -2,7 +2,6 @@ package com.greenbridge.controllers;
 
 import com.greenbridge.entities.Cliente;
 import com.greenbridge.services.ClienteService;
-import com.greenbridge.services.ClienteServiceImpl;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -10,25 +9,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.view.RedirectView;
 
+/** Questa classe gestisce le operazione di autenticazione nel sistema.*/
 @RestController
 @RequestMapping("/api")
 public class LoginController {
 
     @Autowired
-    ClienteServiceImpl clienteService;
+    private ClienteService clienteService;
 
+    /**Questo metodo autentifica un utente cliente.*/
     @PostMapping("/loginCliente")
-    public ResponseEntity<String> saveCliente(@RequestBody Cliente cliente, HttpSession session){
+    public ResponseEntity<String> saveCliente(
+            @RequestBody Cliente cliente,
+            HttpSession session) {
         Cliente c = clienteService.getClienteByEmail(cliente.getEmail());
-        if (c != null && cliente.getPassword().compareTo(c.getPassword()) == 0) {
+        if (c != null &&
+            cliente.getPassword().compareTo(c.getPassword()) == 0) {
             session.setAttribute("cliente", c);
             return new ResponseEntity<>("ok",HttpStatus.OK);
         }
         return new ResponseEntity<>("notok", HttpStatus.FORBIDDEN);
     }
 
+    /**Questo metodo effettua il logout di un utente cliente.*/
     @GetMapping("/logoutCliente")
-    public RedirectView logoutCliente(HttpSession session){
+    public RedirectView logoutCliente(HttpSession session) {
         session.removeAttribute("cliente");
         return new RedirectView("/home");
     }
--- src/main/java/com/greenbridge/entities/Cliente.java
+++ src/main/java/com/greenbridge/entities/Cliente.java
@@ -5,7 +5,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Date;
 
@@ -16,16 +19,27 @@ import java.sql.Date;
 @AllArgsConstructor
 public class Cliente {
 
+    /** Identificativo utente cliente. */
     @Id
     @GeneratedValue (strategy = GenerationType.AUTO)
     private Integer id;
+    /** Nome del cliente. */
     private String nome;
+    /** Cognome del cliente. */
     private String cognome;
+    /** Email del cliente. */
     private String email;
+    /** Password del cliente. */
     private String password;
+    /** Data di nascita del cliente. */
     private Date dataDiNascita;
 
-    public Cliente(String nome, String cognome, String email, String password, Date dataDiNascita) {
+    public Cliente(
+            String nome,
+            String cognome,
+            String email,
+            String password,
+            Date dataDiNascita) {
         this.nome = nome;
         this.cognome = cognome;
         this.email = email;
