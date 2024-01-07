3c1ab2eea908f4b262f235db909d04e549b037f4
davidexvii-uni <d.califano18@studenti.unisa.it>
2024-01-07 11:32:40 +0100

    Risoluzione errori checkstyle

--- src/main/java/com/greenbridge/controllers/ChatbotController.java
+++ src/main/java/com/greenbridge/controllers/ChatbotController.java
@@ -9,7 +9,7 @@ import jakarta.servlet.http.HttpSession;
 @Controller
 public class ChatbotController {
     @GetMapping("/")
-    public String showHome(Model model, HttpSession session){
+    public String showHome(Model model, HttpSession session) {
         session.setAttribute("cliente", "true");
         return "/pages/user/home";
     }
--- src/main/java/com/greenbridge/controllers/ChatbotRestController.java
+++ src/main/java/com/greenbridge/controllers/ChatbotRestController.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
@@ -23,7 +23,9 @@ public class ChatbotRestController {
 
         RestTemplate restTemplate = new RestTemplate();
 
-        String response = restTemplate.exchange(flaskServerUrl, HttpMethod.POST, entity, String.class).getBody();
+        String response = restTemplate.exchange(
+                flaskServerUrl, HttpMethod.POST, entity, String.class).
+                getBody();
 
 
         return "Request sent to Flask server. Response: " + response;
