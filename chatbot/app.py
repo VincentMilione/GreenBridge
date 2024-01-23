b3121d1ecd005d4afbdacae778a54f3835827559
davidexvii-uni <132910696+davidexvii-uni@users.noreply.github.com>
2024-01-23 12:47:56 +0100

    Aggiunta docstring app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -6,12 +6,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost:3306/greenbridgedb'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+
 @app.route('/start-module', methods=['POST'])
 def startModule():
+    """
+Questa funzione gestisce la richiesta POST per avviare il modulo.
+Genera una lista casuale di ID agricoltori e restituisce la risposta JSON contenente l'elenco.
+
+Returns:
+    flask.Response: Risposta JSON contenente l'elenco casuale di ID agricoltori generato.
+"""
+    # Genera una lista casuale di ID agricoltori
     idList = random.sample(range(1, 20), 5)
+
+    # Crea una risposta JSON contenente l'elenco casuale di ID agricoltori
     dati=jsonify({'id_list': idList})
     return dati
-
 
 
 if __name__ == '__main__':
