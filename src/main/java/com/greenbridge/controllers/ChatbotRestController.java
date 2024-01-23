ec95ca2cc6f210086022ed9be3359e6c9c4bc17b
davidexvii-uni <132910696+davidexvii-uni@users.noreply.github.com>
2024-01-23 12:41:32 +0100

    code review ChatbotRestController

--- src/main/java/com/greenbridge/controllers/ChatbotRestController.java
+++ src/main/java/com/greenbridge/controllers/ChatbotRestController.java
@@ -5,7 +5,11 @@ import com.greenbridge.entities.Agricoltore;
 import com.greenbridge.services.AgricoltoreServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.*;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,19 +17,40 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
+/**
+ * Questa classe rappresenta un controller REST
+ * che gestisce le richieste API per il chatbot.
+ * Le operazioni principali riguardano l'esecuzione di comandi
+ * e la gestione delle risposte.
+ *
+ * @author Davide Califano, Giovanni De Gregorio
+ * @version 1.0
+ */
 @RestController
 @RequestMapping("/api")
 public class ChatbotRestController {
-    RestTemplate restTemplate = new RestTemplate();
+    /**
+     * Template per le chiamate REST utilizzato
+     * per comunicare con il server Flask.
+     */
+    private RestTemplate restTemplate = new RestTemplate();
+    /**
+     * Implementazione del servizio AgricoltoreService
+     * utilizzata per ottenere informazioni sugli agricoltori.
+     */
     @Autowired
-    AgricoltoreServiceImpl agricoltoreServiceImpl;
+    private AgricoltoreServiceImpl agricoltoreServiceImpl;
+    /**
+     * Metodo che gestisce le richieste POST relative all'esecuzione di comandi.
+     *
+     * @param command Comando inviato come corpo della richiesta.
+     * @return Lista di agricoltori affini al cliente.
+     */
 
     @PostMapping("/executeCommand")
-    public List<String> executeCommand(@RequestBody String command) {
+    public List<String> executeCommand(@RequestBody final String command) {
         System.out.println(command);
 
         // Controllo sul comando ricevuto
@@ -35,11 +60,12 @@ public class ChatbotRestController {
             headers.setContentType(MediaType.APPLICATION_JSON);
             HttpEntity<String> entity = new HttpEntity<>(headers);
             String flaskServerUrl = "http://127.0.0.1:5000/start-module";
-            RestTemplate restTemplate = new RestTemplate();
 
-            ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(
-                    flaskServerUrl, HttpMethod.POST, entity, new ParameterizedTypeReference<Map<String, Object>>() {
-                    });
+            // Effettua una chiamata REST al server Flask per avviare il modulo
+            ResponseEntity<Map<String, Object>> responseEntity =
+                restTemplate.exchange(flaskServerUrl, HttpMethod.POST, entity,
+                new ParameterizedTypeReference<Map<String, Object>>() {
+                });
             Map<String, Object> responseBody = responseEntity.getBody();
 
             // Ottenere l'oggetto associato alla chiave "id_list"
@@ -54,21 +80,20 @@ public class ChatbotRestController {
                 for (Integer intValue : idList) {
                     System.out.println("Valore intero: " + intValue);
                     agricoltoreServiceImpl.getSingleAgricoltore(intValue);
-                    Agricoltore agricoltore = agricoltoreServiceImpl.getSingleAgricoltore(intValue);
-
-                    agricoltoriList.add(agricoltore.getNomeBottega()); // Aggiungi l'agricoltore alla lista
+                    Agricoltore agricoltore =
+                        agricoltoreServiceImpl.getSingleAgricoltore(intValue);
+                    // Aggiungi l'agricoltore alla lista
+                    agricoltoriList.add(agricoltore.getNomeBottega());
 
                 }
-            }
-            else {
+            } else {
                 // Gestire il caso in cui l'oggetto non è un array
-                System.err.println("La chiave 'arrayDiInt' non contiene un array di interi.");
+                System.err.println("idList non contiene un array di interi.");
             }
 
 
             return agricoltoriList;
-        }
-         else{
+        } else {
             // Comando non riconosciuto
             List<String> error = new ArrayList<>();
             error.add("Comando non riconosciuto");
