c50c82869dc1c4596d95f169735a9e77418cca14
davidexvii-uni <d.califano18@studenti.unisa.it>
2024-01-24 21:42:15 +0100

    cambio di junit da 5 a 4

--- src/test/java/com/greenbridge/unit/TF4.java
+++ src/test/java/com/greenbridge/unit/TF4.java
@@ -1,12 +1,12 @@
 package com.greenbridge.unit;
 
 import com.greenbridge.GreenBridgeApplication;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -14,8 +14,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
-@ExtendWith(SpringExtension.class)
-    @SpringBootTest(classes = GreenBridgeApplication.class) //
+@RunWith(SpringRunner.class)
+    @SpringBootTest(classes = GreenBridgeApplication.class)
     @AutoConfigureMockMvc
     public class TF4 {
         @Autowired
