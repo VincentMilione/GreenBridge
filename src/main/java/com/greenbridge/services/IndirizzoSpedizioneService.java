f17c545ebdff40ee775e70d6afebaf9d32cadd7a
deg997 <128143295+deg997@users.noreply.github.com>
2024-01-27 11:38:47 +0100

    Minor fix css chatbot

--- src/main/java/com/greenbridge/services/IndirizzoSpedizioneService.java
+++ src/main/java/com/greenbridge/services/IndirizzoSpedizioneService.java
@@ -5,7 +5,7 @@ import com.greenbridge.repositories.IndirizzoSpedizioneRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import jakarta.transaction.Transactional;
 
 @Service
 public class IndirizzoSpedizioneService {
